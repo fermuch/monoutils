6ef1876f598c194aada1bd1a8cb16637dd62315a
Fernando Mumbach <fermuch@ayvu.net>
2022-03-10 10:24:59 -0300

    re-setting env before each script

--- private/jest/setup.ts
+++ private/jest/setup.ts
@@ -1,42 +1,46 @@
-// const global: ScriptGlobal;
 
-const _mockStorage = new Map<string, string | number | boolean>();
+// setup global environment
+beforeEach(() => {
+  const _mockStorage = new Map<string, string | number | boolean>();
 
-(global as any).platform = {
-  log(...args: any[]) {
-    console.log(...args);
-  },
-  // phone-only
-  set(key: string, val: string | number | boolean) {
-    _mockStorage.set(key, val);
-  },
-  delete(key: string) {
-    _mockStorage.delete(key);
-  },
-  getString(key: string) {
-    return _mockStorage.get(key) as string;
-  },
-  getBoolean(key: string) {
-    return _mockStorage.get(key) as boolean;
-  },
-  getNumber(key: string) {
-    return _mockStorage.get(key) as number;
-  }
-};
+  // const global: ScriptGlobal;
 
-// const telematree: telematree;
+  (global as any).platform = {
+    log(...args: any[]) {
+      console.log(...args);
+    },
+    // phone-only
+    set(key: string, val: string | number | boolean) {
+      _mockStorage.set(key, val);
+    },
+    delete(key: string) {
+      _mockStorage.delete(key);
+    },
+    getString(key: string) {
+      return _mockStorage.get(key) as string;
+    },
+    getBoolean(key: string) {
+      return _mockStorage.get(key) as boolean;
+    },
+    getNumber(key: string) {
+      return _mockStorage.get(key) as number;
+    }
+  };
 
-(global as any).data = {
-  DEVICE_ID: 'TEST',
-};
+  // const telematree: telematree;
 
-// const env: DynamicData['env'];
-(global as any).env = {}; // TODO!
+  (global as any).data = {
+    DEVICE_ID: 'TEST',
+  };
 
-// const messages: TypedEmitter<EventArgs>;
-// const uuid: v4;
-// const when: FNArgs;
+  // const env: DynamicData['env'];
+  (global as any).env = {}; // TODO!
 
-(global as any).script = undefined;
-(global as any).settings = () => ({});
-(global as any).getSettings = () => ({});
+  // const messages: TypedEmitter<EventArgs>;
+  // const uuid: v4;
+  // const when: FNArgs;
+
+  (global as any).script = undefined;
+  (global as any).settings = () => ({});
+  (global as any).getSettings = () => ({});
+})
