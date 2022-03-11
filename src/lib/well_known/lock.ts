a6474477e196722697b1f6a43e1960e232dc1dcb
Fernando Mumbach <fermuch@ayvu.net>
2022-03-11 16:40:29 -0300

    added getLockState

--- src/lib/well_known/lock.ts
+++ src/lib/well_known/lock.ts
@@ -1,8 +1,16 @@
 import { emit } from "../tools";
 import { BaseEvent } from "./event";
 
+/**
+ * Used in `env.data` to write/read current lock state.
+ */
+export const LOCK_STATE_KEY = 'LOCK_STATE' as const;
+
+/**
+ * Event sent when using lock()/unlock()
+ */
 export class LockEvent extends BaseEvent {
-  kind = "lock-request";
+  public readonly kind = "lock-request" as const;
 
   constructor(public readonly lock: boolean) {
     super();
@@ -15,16 +23,34 @@ export class LockEvent extends BaseEvent {
   }
 }
 
-// Request the device to be locked.
-// An event is dispatched and a handler can be attached to it,
-// to separate the logic of the lock system in use (monoflow, teltonika, ...).
+/**
+ * Request the device to be locked.
+ * An event is dispatched and a handler can be attached to it,
+ * to separate the logic of the lock system in use (monoflow, teltonika, ...).
+ *
+ * @returns true if the message was sent, false otherwise.
+ */
 export function lock(): boolean {
+  env.setData(LOCK_STATE_KEY, true);
   return emit(new LockEvent(true));
 }
 
-// Request the device to unlock.
-// An event is dispatched and a handler can be attached to it,
-// to separate the logic of the lock system in use (monoflow, teltonika, ...).
+/**
+ * Request the device to unlock.
+ * An event is dispatched and a handler can be attached to it,
+ * to separate the logic of the lock system in use (monoflow, teltonika, ...).
+ *
+ * @returns true if the message was sent, false otherwise.
+ */
 export function unlock(): boolean {
+  env.setData(LOCK_STATE_KEY, false);
   return emit(new LockEvent(false));
+}
+
+/**
+ * Get current lock state. This is shared across all instances.
+ * @returns true if the device is locked, false otherwise.
+ */
+export function getLockState(): boolean {
+  return Boolean(env.data?.[LOCK_STATE_KEY] || false);
 }
