887a0d950d704b7f84636bc1d0564cd242e79cb8
Fernando Mumbach <fermuch@ayvu.net>
2022-03-11 14:51:40 -0300

    restoring to even older version of webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
 const path = require('path');
-// const webpack = require('webpack');
+const webpack = require('webpack');
 const PrettierPlugin = require("prettier-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
+const getPackageJson = require('./scripts/getPackageJson');
 
-var babelOptions = {
-  presets: [
-    ['@babel/env', {
-      targets: {
-        android: '4.4',
-        ie: 6,
-      },
-      bugfixes: true,
-      spec: true,
-      modules: false,
-      debug: false,
-      useBuiltIns: false,
-    }],
-  ],
-  "plugins": [["@babel/plugin-transform-arrow-functions", { "spec": true }]]
-};
+const {
+  version,
+  name,
+  license,
+  repository,
+  author,
+} = getPackageJson('version', 'name', 'license', 'repository', 'author');
+
+const banner = `
+  ${name} v${version}
+  ${repository.url}
+
+  Copyright (c) ${author.replace(/ *<[^)]*> */g, " ")} and project contributors.
+
+  This source code is licensed under the ${license} license found in the
+  LICENSE file in the root directory of this source tree.
+`;
 
 module.exports = {
   mode: "production",
   devtool: 'source-map',
   entry: './src/lib/index.ts',
-  target: 'es5',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
     globalObject: `typeof self !== 'undefined' ? self : this`,
     library: "MonoUtils",
     libraryTarget: 'umd',
-    chunkFormat: 'commonjs',
     clean: true
   },
   optimization: {
@@ -45,25 +45,17 @@ module.exports = {
       {
         test: /\.(m|j|t)s$/,
         exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: babelOptions
-          }, {
-            loader: 'ts-loader',
-            options: {
-              allowTsInNodeModules: true
-            },
-          }
-        ],
+        use: {
+          loader: 'babel-loader'
+        }
       },
     ]
   },
   plugins: [
     new PrettierPlugin(),
+    new webpack.BannerPlugin(banner)
   ],
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    fallback: { "stream": false, "http": false, "url": false, "https": false, "zlib": false }
+    extensions: ['.ts', '.js', '.json']
   }
 };
