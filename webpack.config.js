313938a085870e255c6ad7cc35bef59ad2cb60d6
Fernando Mumbach <fermuch@ayvu.net>
2022-03-11 12:13:29 -0300

    checking if self is defined before using it on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build'),
+    globalObject: `typeof self !== 'undefined' ? self : this`,
     library: "MonoUtils",
     libraryTarget: 'umd',
     clean: true
